9b3f387cb7696a393bb74aad998331b47656f1d3
Sven Marnach <sven@marnach.net>
2015-04-07 11:22:53 +0200

    Add grouping of fields to the question admin interface.

--- peerinst/admin.py
+++ peerinst/admin.py
@@ -1,5 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 from . import models
 
-admin.site.register(models.Question)
-admin.site.register(models.Assignment)
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        (_('Main image or video'), {'fields': ['primary_image', 'primary_video_url']}),
+        (_('Secondary image or video'), {
+            'fields': ['secondary_image', 'secondary_video_url'],
+            'classes': ['collapse'],
+            'description': _(
+                'Choose either a video or image to include on the first page of the question, '
+                'where students select concept tags. This is only used if you want the question '
+                'to be hidden when students select concept tags; instead, a preliminary video or '
+                'image can be displayed. The main question image will be displayed on all '
+                'subsequent pages.'
+            ),
+        }),
+        (_('Answers'), {'fields': [
+            'answer_style', 'answer_num_choices', 'correct_answer', 'second_best_answer'
+        ]}),
+        (None, {'fields': ['example_rationale']}),
+    ]
+
+@admin.register(models.Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    pass
