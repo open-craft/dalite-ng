59925edd334cc4aafa5f48e3a412108ba7e0e24a
Sven Marnach <sven@marnach.net>
2015-04-15 12:25:32 +0200

    Refactor views using class-based views.

--- peerinst/urls.py
+++ peerinst/urls.py
@@ -5,11 +5,11 @@ from . import views
 urlpatterns = [
     url(r'^$', views.AssignmentListView.as_view(), name='assignment-list'),
     url(r'^assignment/(?P<assignment_id>[^/]+)/', include([
-        url(r'^$', views.question, name='start-assignment'),
+        url(r'^$', views.QuestionStartView.as_view(), name='assignment-start'),
         url(r'(?P<question_index>\d+)/', include([
-            url(r'^$', views.question, name='start-question'),
-            url(r'^review$', views.review_answer, name='review-answer'),
-            url(r'^summary$', views.answer_summary, name='answer-summary'),
+            url(r'^$', views.QuestionStartView.as_view(), name='question-start'),
+            url(r'^review$', views.QuestionReviewView.as_view(), name='question-review'),
+            url(r'^summary$', views.QuestionSummaryView.as_view(), name='question-summary'),
         ])),
     ])),
 ]
--- peerinst/views.py
+++ peerinst/views.py
@@ -1,147 +1,190 @@
-from django.http import Http404
-from django.shortcuts import redirect, render
+from django import http
+from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.translation import ugettext_lazy as _
-from django.views.generic.list import ListView
+from django.views.generic import edit
+from django.views.generic import list as list_views
 from . import forms
 from . import models
 
-class AssignmentListView(ListView):
+
+class AssignmentListView(list_views.ListView):
+    """List of assignments used for debugging purposes."""
     model = models.Assignment
 
-def get_lti_user(request):
-    # TODO(smarnach): Return user token of current LTI user
-    return 'test'
 
-def get_question(assignment_id, question_index):
-    """Retrieve the assignment and question for the current request from the database."""
-    try:
-        assignment = models.Assignment.objects.get(identifier=assignment_id)
-    except models.Assignment.DoesNotExist:
-        raise Http404(_('Assignment does not exist'))
-    try:
-        # We use one-based indexing in the public interface, so we have to subtract 1.
-        question = assignment.questions.all()[int(question_index) - 1]
-    except IndexError:
-        raise Http404(_('Question does not exist'))
-    return assignment, question
+class QuestionRedirect(Exception):
+    """Raised to cause a redirect to target url within the question views."""
 
-def question(request, assignment_id, question_index='1'):
+    def __init__(self, target_url_name):
+        self.target_url_name = target_url_name
+
+
+class QuestionView(edit.FormView):
+    """Base class for the views in the student UI."""
+
+    def get_form_kwargs(self):
+        self.assignment_id = self.kwargs['assignment_id']
+        self.question_index = int(self.kwargs.get('question_index', 1))
+        self.assignment = get_object_or_404(models.Assignment, identifier=self.assignment_id)
+        try:
+            # We use one-based indexing in the public interface, so we have to subtract 1.
+            self.question = self.assignment.questions.all()[int(self.question_index) - 1]
+        except IndexError:
+            raise http.Http404(_('Question does not exist.'))
+        return edit.FormView.get_form_kwargs(self)
+
+    def get_context_data(self, **kwargs):
+        context = edit.FormView.get_context_data(self, **kwargs)
+        context.update(
+            question_index=self.question_index,
+            assignment=self.assignment,
+            question=self.question,
+        )
+        return context
+
+    def get_redirect_url(self, name):
+        return reverse(
+            name,
+            kwargs=dict(assignment_id=self.assignment_id, question_index=self.question_index),
+        )
+
+    def get_success_url(self):
+        return self.get_redirect_url(self.success_url_name)
+
+    def get(self, request, *args, **kwargs):
+        # We override this method to enable triggering redirects from within other methods, which
+        # is usually not possible.
+        try:
+            return edit.FormView.get(self, request, *args, **kwargs)
+        except QuestionRedirect as e:
+            return redirect(self.get_redirect_url(e.target_url_name))
+
+    def start_over(self):
+        """Start over with the current question.
+
+        This redirect is used when incosistent data is encountered and shouldn't be called under
+        normal circumstances.
+        """
+        # TODO(smarnach): Push some error message for display at the top of the page to notify
+        # user that something went wrong.  Maybe we should show a "Bad Request" error page instead.
+        raise QuestionRedirect('question-start')
+
+
+class QuestionStartView(QuestionView):
     """Render a question with answer choices.
 
     The user can choose one answer and enter a rationale.
     """
-    assignment, question = get_question(assignment_id, question_index)
-    form = forms.FirstAnswerForm(request.POST or None, question=question)
-    if form.is_valid():
-        request.session['answer_dict'] = dict(
+
+    template_name = 'peerinst/question.html'
+    form_class = forms.FirstAnswerForm
+    success_url_name = 'question-review'
+
+    def get_form_kwargs(self):
+        kwargs = QuestionView.get_form_kwargs(self)
+        kwargs.update(question=self.question)
+        return kwargs
+
+    def form_valid(self, form):
+        self.request.session['answer_dict'] = dict(
             first_answer_choice=form.cleaned_data['first_answer_choice'],
             rationale=form.cleaned_data['rationale'],
         )
-        return redirect(
-            'review-answer', assignment_id=assignment_id, question_index=question_index
-        )
-    context = dict(
-        question_index=question_index,
-        assignment=assignment,
-        question=question,
-        form=form,
-    )
-    return render(request, 'peerinst/question.html', context)
+        return QuestionView.form_valid(self, form)
 
-def review_answer(request, assignment_id, question_index):
+
+class QuestionReviewView(QuestionView):
     """Show rationales from other users and give the opportunity to reconsider the first answer."""
-    assignment, question = get_question(assignment_id, question_index)
-    try:
-        answer_dict = request.session['answer_dict']
-        first_answer_choice = answer_dict['first_answer_choice']
-        context = dict(
-            question_index=question_index,
-            assignment=assignment,
-            question=question,
-            first_choice_label=question.get_choice_label(first_answer_choice),
-            rationale=answer_dict['rationale'],
+
+    template_name = 'peerinst/review_answer.html'
+    form_class = forms.ReviewAnswerForm
+    success_url_name = 'question-summary'
+
+    def get_form_kwargs(self):
+        try:
+            self.answer_dict = self.request.session['answer_dict']
+            self.first_answer_choice = self.answer_dict['first_answer_choice']
+            self.rationale = self.answer_dict['rationale']
+        except KeyError:
+            # We got here without doing the first step, or the session has expired.
+            self.start_over()
+        kwargs = QuestionView.get_form_kwargs(self)
+        kwargs.update(question=self.question, first_answer_choice=self.first_answer_choice)
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        context = QuestionView.get_context_data(self, **kwargs)
+        context.update(
+            first_choice_label=self.question.get_choice_label(self.first_answer_choice),
+            rationale=self.rationale,
         )
-    except KeyError:
-        # We got here without doing the first step, or the session has expired.  Let's start over.
-        return redirect(
-            'start-question', assignment_id=assignment_id, question_index=question_index
-        )
-    form = forms.ReviewAnswerForm(
-        request.POST or None, question=question, first_answer_choice=first_answer_choice
-    )
-    if form.is_valid():
-        answer_dict.update(
+        return context
+
+    def form_valid(self, form):
+        self.answer_dict.update(
             second_answer_choice=form.cleaned_data['second_answer_choice'],
             chosen_rationale_id=form.cleaned_data['chosen_rationale_id'],
         )
-        request.session['answer_dict'] = answer_dict
-        return redirect(
-            'answer-summary', assignment_id=assignment_id, question_index=question_index
-        )
-    context.update(form=form)
-    return render(request, 'peerinst/review_answer.html', context)
+        self.request.session['answer_dict'] = self.answer_dict
+        return QuestionView.form_valid(self, form)
 
-class InvalidAnswer(Exception):
-    """Raised when the data in the answer_dict in the session is inconsistent or incomplete.
 
-    This shouldn't happen unless e.g. the rationale the user chose was removed from the DB in the
-    meantime.
-    """
+class QuestionSummaryView(QuestionView):
+    """Show a summary of answers to the student and submit the data to the database."""
 
-def save_answer(request, question, answer_dict):
-    """Validate and save the answer defined by the arguments to the database."""
-    try:
-        chosen_rationale = models.Answer.objects.get(id=answer_dict['chosen_rationale_id'])
-    except models.Answer.DoesNotExist:
-        raise InvalidAnswer
-    if chosen_rationale.first_answer_choice != answer_dict['second_answer_choice']:
-        raise InvalidAnswer
-    answer = models.Answer(
-        question=question,
-        first_answer_choice=answer_dict['first_answer_choice'],
-        rationale=answer_dict['rationale'],
-        second_answer_choice=answer_dict['second_answer_choice'],
-        chosen_rationale=chosen_rationale,
-        user_token=get_lti_user(request)
-    )
-    answer.save()
+    template_name = 'peerinst/answer_summary.html'
+    form_class = forms.forms.Form
+    success_url_name = 'question-start'
 
-def answer_summary(request, assignment_id, question_index):
-    assignment, question = get_question(assignment_id, question_index)
-    try:
-        answer_dict = request.session['answer_dict']
-        first_answer_choice = answer_dict['first_answer_choice']
-        second_answer_choice = answer_dict['second_answer_choice']
-        context = dict(
-            question_index=question_index,
-            assignment=assignment,
-            question=question,
-            first_choice_label=question.get_choice_label(first_answer_choice),
-            rationale=answer_dict['rationale'],
-            second_choice_label=question.get_choice_label(second_answer_choice),
-            chosen_rationale_id=answer_dict['chosen_rationale_id'],
-        )
-    except KeyError:
-        # We got here without doing the first steps, or the session has expired.  Let's start over.
-        return redirect(
-            'start-question', assignment_id=assignment_id, question_index=question_index
-        )
-    if request.method == 'POST':
+    def get_form_kwargs(self):
         try:
-            save_answer(request, question, answer_dict)
-        except InvalidAnswer:
-            # The answer was inconsistent for some reason.  Let's start over with this question,
-            # since this can't happen in the normal flow.
-            # TODO(smarnach): Push some error message for display at the top of the page to notify
-            # user that something went wrong.
-            pass
-        else:
-            question_index = int(question_index) + 1
-            if question_index >= assignment.questions.count():
-                # TODO(smarnach): Figure out what to do when reaching the last question.
-                return redirect('assignment-list')
-        del request.session['answer_dict']
-        return redirect(
-            'start-question', assignment_id=assignment_id, question_index=question_index
+            self.answer_dict = self.request.session['answer_dict']
+            self.first_answer_choice = self.answer_dict['first_answer_choice']
+            self.second_answer_choice = self.answer_dict['second_answer_choice']
+            self.rationale = self.answer_dict['rationale']
+            self.chosen_rationale_id = self.answer_dict['chosen_rationale_id']
+        except KeyError:
+            # We got here without doing the first steps, or the session has expired.
+            self.start_over()
+        return QuestionView.get_form_kwargs(self)
+
+    def get_context_data(self, **kwargs):
+        context = QuestionView.get_context_data(self, **kwargs)
+        context = dict(
+            first_choice_label=self.question.get_choice_label(self.first_answer_choice),
+            second_choice_label=self.question.get_choice_label(self.second_answer_choice),
+            rationale=self.rationale,
         )
-    return render(request, 'peerinst/answer_summary.html', context)
+        return context
+
+    def form_valid(self, form):
+        self.save_answer()
+        del self.request.session['answer_dict']
+        self.question_index += 1
+        if self.question_index >= self.assignment.questions.count():
+            # TODO(smarnach): Figure out what to do when reaching the last question.
+            return redirect('assignment-list')
+        return QuestionView.form_valid(self, form)
+
+    def save_answer(self):
+        """Validate and save the answer defined by the arguments to the database."""
+        try:
+            chosen_rationale = models.Answer.objects.get(id=self.chosen_rationale_id)
+        except models.Answer.DoesNotExist:
+            self.start_over()
+        if chosen_rationale.first_answer_choice != self.second_answer_choice:
+            self.start_over()
+        answer = models.Answer(
+            question=self.question,
+            first_answer_choice=self.first_answer_choice,
+            rationale=self.rationale,
+            second_answer_choice=self.second_answer_choice,
+            chosen_rationale=chosen_rationale,
+            user_token=self.get_user_token(),
+        )
+        answer.save()
+
+    def get_user_token(self):
+        # TODO(smarnach): Return user token of current LTI user
+        return 'test'
