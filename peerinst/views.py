f73f4710c3ff7a64feb29d3348f2cdd17ec85e39
Jonathon Sumner <jonathonresumner@gmail.com>
2018-01-19 16:14:04 -0500

    question analytics added to list view.  confusion matrix calculated as class method.  slow.

--- peerinst/views.py
+++ peerinst/views.py
@@ -75,6 +75,7 @@ class QuestionListView(LoginRequiredMixin, ListView):
 
 
 class QuestionMixin(object):
+
     def get_context_data(self, **kwargs):
         context = super(QuestionMixin, self).get_context_data(**kwargs)
         context.update(
