a5b2afe89c3b585542fab0106658efcf3e421490
Jonathon Sumner <jonathonresumner@gmail.com>
2018-04-13 22:08:14 -0400

    custom templatetag to modify class attribute

--- peerinst/templatetags/add_class.py
+++ peerinst/templatetags/add_class.py
@@ -1,13 +1,61 @@
 from django.template import Library
 from django.template.defaultfilters import stringfilter
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
 
 register = Library()
 
 @register.filter(needs_autoescape=True)
 @stringfilter
-def add_class(tag, css_class, autoescape=True):
+def add_class(html, css_class, autoescape=True):
     """Appends given css class to html tag"""
 
-    print(tag)
-    print(css_class)
-    return
+    from HTMLParser import HTMLParser
+
+    if autoescape:
+        html = conditional_escape(html)
+        css_class = conditional_escape(css_class)
+
+    class CustomTagParser(HTMLParser):
+        new_tag = ''
+
+        def handle_starttag(self, tag, attrs):
+            class_added = False
+            self.new_tag = self.new_tag + '<' + tag
+            for key, value in attrs:
+                if key == 'class':
+                    self.new_tag = self.new_tag + ' ' + key + '=\"' + value + ' ' + css_class + '\" '
+                    class_added = True
+                else:
+                    self.new_tag = self.new_tag + ' ' + key + '=\"' + value + '\" '
+
+            if not class_added:
+                self.new_tag = self.new_tag + 'class=\"' + css_class + '\" '
+
+            self.new_tag = self.new_tag + '>'
+
+            return
+
+        def handle_startendtag(self, tag, attrs):
+            self.new_tag = self.new_tag.rstrip('>') + tag
+
+            return
+
+        def handle_data(self, data):
+            self.new_tag = self.new_tag + data
+
+            return
+
+        def handle_endtag(self, tag):
+            self.new_tag = self.new_tag + '</' + tag + '>'
+
+            return
+
+        def processed_tag(self):
+
+            return new_tag
+
+    parser = CustomTagParser()
+    parser.feed(html)
+
+    return mark_safe(parser.new_tag)
