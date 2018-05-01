acece73bd418a38727a211e8a30659840a05d0b4
Jonathon Sumner <jonathonresumner@gmail.com>
2018-05-01 16:21:03 -0400

    removed bug and typo

--- peerinst/urls.py
+++ peerinst/urls.py
@@ -62,7 +62,7 @@ urlpatterns = [
     url(r'^welcome/$', views.welcome, name='welcome'),
     url(r'^password_change/$', password_views.password_change, name='password_change'),
     url(r'^password_change/done/$', auth_views.password_change_done, name='password_change_done'),
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset', html_email_template_name='regristration/password_reset_email_html.html'),
+    url(r'^password_reset/$', auth_views.password_reset, { 'html_email_template_name':'registration/password_reset_email_html.html' }, name='password_reset'),
     url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
